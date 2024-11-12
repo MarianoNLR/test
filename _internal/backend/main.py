b58f0f032ebdab86f5e0806870d964c610263f5b
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-11-12 16:40:03 -0300

    xd2

--- _internal/backend/main.py
+++ _internal/backend/main.py
@@ -9,37 +9,43 @@ from routers import clients, payments, reports, reservations, services, workers
 app = FastAPI()
 
 
-# Configuración de CORS
+       # Configuración de CORS
 origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://127.0.0.1",
-    "http://127.0.0.1:8000",
+    "http://localhost/",
+    "http://localhost:8000/",
+    "http://127.0.0.1/",
+    "http://127.0.0.1:8000/",
 ]
 
-app.add_middleware(
+app.addmiddleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins temporarily
+    alloworigins=[""],  # Allow all origins temporarily
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=[""],
+    allowheaders=["*"],
 )
 
 
-app.include_router(clients.router)
-app.include_router(services.router)
-app.include_router(reservations.router)
+app.includerouter(clients.router)
+app.includerouter(services.router)
+app.includerouter(reservations.router)
 app.include_router(payments.router)
 app.include_router(reports.router) 
 app.include_router(workers.router)
 
-# Sirviendo archivos estáticos desde "web"
-app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "../web")), name="static")
+    # Sirviendo archivos estáticos desde "web"
+app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file), "../web")), name="static")
 
-# Directorio de plantillas (HTML dinámico)
+Directorio de plantillas (HTML dinámico)
 templates = Jinja2Templates(directory="web")
 
 @app.get("/")
 async def read_index(request: Request):
     # Puedes pasar datos dinámicos al template
     return templates.TemplateResponse("index3.html", {"request": request, "message": "Bienvenido a Spa Sentirse Bien"})
+def main():
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name == "__main":
+    main()

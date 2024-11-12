f178974e6e45e0648388d4ef6fabf7742858e150
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-11-12 16:58:38 -0300

    xd3

--- _internal/backend/main.py
+++ _internal/backend/main.py
@@ -3,13 +3,11 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from routers import clients, payments, reports, reservations, services, workers 
-
+from routers import clients, payments, reports, reservations, services, workers
 
 app = FastAPI()
 
-
-       # Configuración de CORS
+# Configuración de CORS
 origins = [
     "http://localhost/",
     "http://localhost:8000/",
@@ -17,35 +15,36 @@ origins = [
     "http://127.0.0.1:8000/",
 ]
 
-app.addmiddleware(
+app.add_middleware(
     CORSMiddleware,
-    alloworigins=[""],  # Allow all origins temporarily
+    allow_origins=origins,  # Permitir solo los orígenes listados
     allow_credentials=True,
-    allow_methods=[""],
-    allowheaders=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-
-app.includerouter(clients.router)
-app.includerouter(services.router)
-app.includerouter(reservations.router)
+# Registrar routers
+app.include_router(clients.router)
+app.include_router(services.router)
+app.include_router(reservations.router)
 app.include_router(payments.router)
-app.include_router(reports.router) 
+app.include_router(reports.router)
 app.include_router(workers.router)
 
-    # Sirviendo archivos estáticos desde "web"
-app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file), "../web")), name="static")
+# Sirviendo archivos estáticos desde "web"
+app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "../web")), name="static")
 
-Directorio de plantillas (HTML dinámico)
+# Directorio de plantillas (HTML dinámico)
 templates = Jinja2Templates(directory="web")
 
 @app.get("/")
 async def read_index(request: Request):
     # Puedes pasar datos dinámicos al template
     return templates.TemplateResponse("index3.html", {"request": request, "message": "Bienvenido a Spa Sentirse Bien"})
+
 def main():
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-if __name == "__main":
+if __name__ == "__main__":
     main()
